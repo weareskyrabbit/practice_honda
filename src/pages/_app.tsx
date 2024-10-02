1623ca3278eac265944e7028546fc5db928d19d1
tf-intern-honda <w.honda.p@p1.techfirm.co.jp>
2024-10-02 16:15:16 +0900

    STEP3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
+import { MarkGameProvider } from "@/providers/MarkGameProvider";
+import "./globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // ここを変更する
+  return <MarkGameProvider>
+      <Component {...pageProps} />
+  </MarkGameProvider>;
 }
